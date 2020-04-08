edbfdc83ad2b04a61e848f7ec6e7f059ac6ced48
Chris Lee <chrislee910320@gmail.com>
2020-04-07 22:26:12 -0600

    removed dummy text

--- client/app/components/HelloWorld/HelloWorld.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const HelloWorld = () => {
-  return <div>Hello Worlddfdfd32</div>
-};
-
-export default HelloWorld;
--- client/app/components/Home/Home.js
+++ client/app/components/Home/Home.js
@@ -8,60 +8,70 @@ import {
 } from "react-router-dom";
 
 
-import App from '../App/App'
-import NotFound from '../App/NotFound';
-
-import Test from './Test';
-
-import HelloWorld from '../HelloWorld/HelloWorld';
-
-
 class Home extends Component {
   constructor(props) {
     super(props);
 
-    // this.state = {
-    //   error: null,
-    //   users: []
-    // };
+    this.state = {
+      error: null,
+      users: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://localhost/myApartment/phpinfo.php", {
+      headers: {
+        'Accept': 'application/json'
+      }
+    })
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            users: [result]
+          });
+
+        },
+        (error) => {
+          this.setState({ error });
+        }
+
+      );
 
   }
 
-  // componentDidMount() {
-  //   fetch("http://localhost/myApartment/phpinfo.php", {
-  //     headers: {
-  //       'Accept': 'application/json'
-  //     }
-  //   })
-  //     .then(res => res.json())
-  //     .then(
-  //       (result) => {
-  //         this.setState({
-  //           users: [result]
-  //         });
-
-  //       },
-  //       (error) => {
-  //         this.setState({ error });
-  //       }
-
-  //     );
-
-  // }
-
   render() {
-    // console.log(this.state.users, "chris");
+    console.log(this.state.users, "chris");
     return (
       <div>
 
         <p className="mainText">JUST React</p>
-        {/* <button onClick={this.getPHP}>load</button> */}
+        <button onClick={this.getPHP}>load</button>
         <h1> Users </h1>
-        {/* {this.state.users.map((user) => (
+        {this.state.users.map((user) => (
 
           < div key={user.id} > {user.lastName} {user.firstName} {user.phone} {user.email}</div>
         ))
-        } */}
+        }
+
+        <h1> contact form </h1>
+
+        <p>Contact Me</p>
+        <div>
+          <form action="/phpinfo.php">
+            <label>First Name</label>
+            <input type="text" id="fname" name="firstname" placeholder="Your name.." />
+            <label>Last Name</label>
+            <input type="text" id="lname" name="lastname" placeholder="Your last name.." />
+
+            <label>Email</label>
+            <input type="email" id="email" name="email" placeholder="Your email" />
+
+            <label>Subject</label>
+            <textarea id="subject" name="subject" placeholder="Write something.."></textarea>
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
 
 
       </div>
